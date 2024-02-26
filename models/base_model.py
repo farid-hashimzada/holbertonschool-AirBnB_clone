2ae5c5c617ebc3cb4d52bb610720f2addc78dbf6
Dadash <huseynzade.dadas@gmail.com>
2024-02-26 20:53:33 +0400

    piece of the serialization/deserialization process

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,29 @@
 #!/usr/bin/python3
-"""BaseModel class"""
+"""Base class"""
+import uuid
+from datetime import datetime
 
 
-class Base:
+class BaseModel:
     """Base class"""
-    pass
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Updates updated attr"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns dic containing all keys/values"""
+        result = {}
+        for key, value in self.__dict__.items():
+            if key == "updated_at" or key == "created_at":
+                value = datetime.isoformat(value)
+            result[key] = value
+        result["__class__"] = self.__class__.__name__
+        return result
