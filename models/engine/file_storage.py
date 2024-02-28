4329c2f0e2454cecc2ce8f199a8a7f8c7a614795
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-02-28 17:40:05 +0400

    save method with storage and update save method test cases

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,10 +14,12 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
+        """ Add object to objects"""
         key = obj.__class__.__name__ + "." + obj.id
         self.__objects[key] = obj
 
     def save(self):
+        """ Save objects to file"""
         ser_object = {}
         for key, obj in self.__objects.items():
             ser_object[key] = obj.to_dict()
@@ -25,6 +27,7 @@ class FileStorage:
             json.dump(ser_object, file)
 
     def reload(self):
+        """ Reload objects from file"""
         try:
             with open(self.__file_path, "r") as file:
                 file_content = file.read()
